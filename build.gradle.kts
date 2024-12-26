dbaf86bb26929148dfcef139ee5245c54eae5303
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-12-25 22:31:18 -0500

    Extra init commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
+    id("maven-publish")
+    java
 }
 
-group = "io.github.arkosammy12"
-version = "1.0-SNAPSHOT"
+group = property("maven_group")!!
+version = property("project_version")!!
+val archiveBaseName: String = property("archives_base_name") as String
+
+base {
+    archivesName = archiveBaseName
+}
 
 repositories {
     mavenCentral()
@@ -11,6 +18,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.xerial:sqlite-jdbc:${property("sqlite-jdbc_version")}")
 }
 
 tasks.test {

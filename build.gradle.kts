8ef97e64f509aad4fc4c1fc751f2f44049177015
ArkoSammy12 <jamesvillarreal12@gmail.com>
2025-03-24 19:24:51 -0500

    Update Kotlin version and set up maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.20"
     id("maven-publish")
     java
 }
@@ -26,4 +26,58 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    repositories {
+        maven("https://maven.enjarai.dev/releases") {
+            name = "enjaraiMaven"
+            credentials(PasswordCredentials::class.java)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = project.group.toString()
+            artifactId = base.archivesName.get()
+            version = project.version.toString()
+            from(components["java"])
+
+            pom {
+                name.set(archiveBaseName)
+                description.set("A declarative Kotlin library to create and manage configuration files easily and idiomatically.") // Change here
+                url.set("https://github.com/ArkoSammy12/monkey-config")
+
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("https://mit-license.org/")
+                    }
+                }
+
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("https://github.com/ArkoSammy12/monkey-config/issues")
+                }
+
+                developers {
+                    developer {
+                        id.set("ArkoSammy12")
+                        //name.set("")
+                        //email.set("")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com:ArkoSammy12/monkey-config.git")
+                    developerConnection.set("scm:git:ssh://github.com:ArkoSammy12/monkey-config.git")
+                    url.set("https://github.com/ArkoSammy12/monkey-config")
+                }
+            }
+
+        }
+    }
 }
---
+++ src/main/kotlin/io/github/arkosammy12/monkeydatabase/Main.kt
@@ -0,0 +1,9 @@
+package io.github.arkosammy12.monkeydatabase
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello world!")
+    }
+}
